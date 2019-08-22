b8819f4b4818f329aaaa3ef48075c1bff0f2d83b
Federico Teotini <templarblack.fed03@gmail.com>
2019-08-22 16:07:11 +0200

    Init

---
+++ src/main/java/com/fed03/app/AnnTest.java
@@ -0,0 +1,12 @@
+package com.fed03.app;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD,ElementType.TYPE})
+public @interface AnnTest{
+    String developer() default "Unknown";
+}
